6cc42e2b97d2ef45357a3cc7af03eb0c424af85b
barbimt <82407633+barbimt@users.noreply.github.com>
2022-05-16 17:13:03 -0300

    pages & routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,29 @@
+
+function About() {
+  return (
+    <>
+    <h1 className='text-6xl mb-4'>Github Finder</h1>
+    <p className='mb-4 text-2xl font-light'>
+      A React app to search GitHub profiles and see profile details. This
+      project is part of the
+      <strong>
+      <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
+        {' '}
+        React Front To Back
+      </a>{' '}
+      </strong>
+      Udemy course by
+      <strong>
+        <a href='https://traversymedia.com'> Brad Traversy</a>
+      </strong>
+      .
+    </p>
+    <p className='text-lg text-gray-400'>
+      Version <strong><span >1.0.0</span> </strong>
+    </p>
+    
+  </>
+  )
+}
+
+export default About

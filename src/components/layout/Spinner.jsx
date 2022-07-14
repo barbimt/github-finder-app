824b0f41e77aee676005f07dede8124d80d68ff0
Barbi Torres <barbi.mtorres@gmail.com>
2022-07-14 16:22:04 -0300

    regenerate token

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,13 @@
+function Spinner() {
+    return (
+      <div className="lds-ellipsis" >
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
+  
+  export default Spinner;
+  
